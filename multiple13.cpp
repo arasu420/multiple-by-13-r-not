43f0c0ffd71921552ccef378e216a8367ff5c2de
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-02-27 21:41:12 +0530

    Create multiple13.cpp

---
+++ multiple13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  int n;
+  cout << "Enter the number";
+  cin>>n;
+  if(n%13==0)
+  {cout<<"yes";}
+  else
+  {cout<<"no";}
+}
